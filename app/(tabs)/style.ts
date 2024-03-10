6760fdc28aca2b8f8c939bf53c1366b89a765335
Mikeal <77624159+Michallesss@users.noreply.github.com>
2024-03-10 22:42:19 +0100

    External styles

---
+++ app/(tabs)/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
+
+export default style;
